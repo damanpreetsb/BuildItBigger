6d3b0bb774f25998d14540ec9444873c02dc6b3e
Daman Singh <singhdaman4321@gmail.com>
2016-10-28 13:58:54 +0530

    -screenshots

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,65 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+    InterstitialAd mInterstitialAd;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
+
+        return root;
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("AA37CAB351D645D2B79AC6AD2D158791")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        }
+    }
+}
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,31 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        return root;
+    }
+}
